b91020ef9aa52cdcb105eb4545996d89c4a82186
MatiasBoldini <matiboldini2000@gmail.com>
2019-06-11 23:01:40 -0300

    Se agrego funcion en javascript para calcular superficie del rectangulo

--- Templates/js/misFunciones.js
+++ Templates/js/misFunciones.js
@@ -1,3 +1,7 @@
-$(document).ready(function(){
-    $('.parallax').parallax();
-});
+function supRect(){
+    var superRect = document.getElementById("SRectanguloInput");
+    var Base = parseInt(document.getElementById("Valor_BRectangulo").value);
+    var Altura = parseInt(document.getElementById("Valor_ARectangulo").value);
+    var superficie = (Base * Altura);
+    superRect.value = superficie;
+}
