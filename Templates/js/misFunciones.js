4d860512a032a4685d5360a9a5e3a80b7dd1bb45
MatiasBoldini <matiboldini2000@gmail.com>
2019-06-11 23:06:11 -0300

    Se agrego funcion js para superficie del triangulo

--- Templates/js/misFunciones.js
+++ Templates/js/misFunciones.js
@@ -5,3 +5,11 @@ function supRect(){
     var superficie = (Base * Altura);
     superRect.value = superficie;
 }
+
+function supTrian(){
+    var superTrian = document.getElementById("STrianguloInput");
+    var Base = parseInt(document.getElementById("Valor_BTriangulo").value);
+    var Altura = parseInt(document.getElementById("Valor_ATriangulo").value);
+    var superficie = (Base * Altura) / 2;
+    superTrian.value = superficie;
+}
